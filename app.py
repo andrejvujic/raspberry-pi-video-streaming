2c889edc88707a2d76974a1b8ec0b7240f362bb6
Andrej Vujić <vujicandrej365@gmail.com>
2022-08-03 15:27:54 +0200

    store flip values in VideoCamera class

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, Response, render_template, request
+from flask import Flask, Response, render_template
 from camera import VideoCamera
 
 camera = VideoCamera()
 app = Flask(__name__)
 
 
-def _gen(camera: VideoCamera, flip_h: bool, flip_v: bool):
+def _gen(camera: VideoCamera):
     while True:
-        frame = camera.get_frame(flip_h=flip_h, flip_v=flip_v)
+        frame = camera.get_frame()
         yield (
             b"--frame\r\n"
             b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
@@ -21,13 +21,9 @@ def index():
 
 @app.route("/video", methods=["GET"])
 def video():
-    args = request.args
-    flip_h = args.get("flip_h", default=False)
-    flip_v = args.get("flip_v", default=False)
-
     return Response(
         _gen(
-            camera=camera, flip_h=flip_h, flip_v=flip_v,
+            camera=camera,
         ),
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
--- camera.py
+++ camera.py
@@ -5,27 +5,29 @@ import numpy as np
 
 
 class VideoCamera:
-    def __init__(self, type: str = ".jpg", index: int = -1) -> None:
+    def __init__(self, type: str = ".jpg", index: int = -1, flip_h: bool = False, flip_v: bool = False) -> None:
         self.index = index
         self.cap = cv2.VideoCapture(self.index)
         self.type = type
+        self.flip_h = flip_h
+        self.flip_v = flip_v
         time.sleep(2.0)
 
     def __del__(self) -> None:
         self.cap.release()
 
-    def apply_flips(self, frame: Any, flip_h: bool = False, flip_v: bool = False) -> Any:
-        if flip_h:
+    def apply_flips(self, frame: Any) -> Any:
+        if self.flip_h:
             frame = np.fliplr(frame)
 
-        if flip_v:
+        if self.flip_v:
             frame = np.flip(frame, 0)
 
         return frame
 
-    def get_frame(self, flip_h=True, flip_v=True) -> Any:
+    def get_frame(self) -> Any:
         _, frame = self.cap.read()
-        frame = self.apply_flips(frame=frame, flip_h=flip_h, flip_v=flip_v)
+        frame = self.apply_flips(frame=frame)
         _, image = cv2.imencode(self.type, frame)
 
         return image.tobytes()
